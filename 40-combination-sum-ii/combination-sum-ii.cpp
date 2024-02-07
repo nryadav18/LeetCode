b79dce3927ef3da65a836bc9bb9cd8f70a94d1c1
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-02-07 17:59:16 +0530

    Time: 185 ms (5.10%) | Memory: 126.5 MB (5.01%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    set<vector<int>>sov;
+    void recFunCall(vector<int>nums,int i,vector<int>v,int n,int t,int sum){
+        if (i >= n and sum != t){
+            return ;
+        }
+        else if (sum == t){
+            sov.insert(v);
+            return;
+        }
+        else if (sum > t){
+            return;
+        }
+        v.push_back(nums[i]);
+        recFunCall(nums,i+1,v,n,t,sum+nums[i]);
+        while (i+1 < n and nums[i]==nums[i+1]) i++;
+        v.pop_back();
+        recFunCall(nums,i+1,v,n,t,sum);
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<int>v;
+        recFunCall(nums,0,v,n,target,0);
+        vector<vector<int>>res;
+        for (auto it : sov){
+            vector<int>temp;
+            for (auto ls : it){
+                temp.push_back(ls);
+            }
+            res.push_back(temp);
+        }
+        return res;
+    }
+};
