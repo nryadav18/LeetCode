d3c7d524782cfae429e41c2c84d5baf11c8e5347
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-02-22 22:22:02 +0530

    Time: 135 ms (65.93%) | Memory: 72.3 MB (28.15%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<vector<int>>vov(n+1,vector<int>(2,0));
+        vector<pair<int,int>>vov(n+1);
         for (auto it : trust){
-            vov[it[0]][1]++;
-            vov[it[1]][0]++;
+            vov[it[0]].second++;
+            vov[it[1]].first++;
         }
         for (int i=1;i<=n;i++){
-            if (vov[i][0] == n-1 and vov[i][1] == 0){
+            if (vov[i].first == n-1 and vov[i].second == 0){
                 return i;
             }
         }
