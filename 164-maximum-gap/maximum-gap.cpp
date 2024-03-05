2710731579b37c832f7171170fe9b67c8fea85e1
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-03-05 22:16:25 +0530

    Time: 153 ms (48.36%) | Memory: 85.6 MB (49.95%) - LeetSync

---
+++ 164-maximum-gap/maximum-gap.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int maxi = 0;
+        for (int i=0;i<n-1;i++) if (nums[i+1] - nums[i] > maxi) maxi = nums[i+1] - nums[i];
+        return maxi;
+    }
+};
