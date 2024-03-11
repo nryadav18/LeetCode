1204f04a68fd797179b97d74dc0808d864a0bbcf
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-03-11 23:30:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (66.00%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int>v1(26,0),v2(26,0);
+        for (auto it : order){
+            v1[it - 'a']++;
+        }
+        for (auto it : s){
+            v2[it - 'a']++;
+        }
+        string jyo;
+        for (auto it : order){
+            if (v1[it - 'a'] == 1 and v2[it - 'a'] >= 1){
+                while (v2[it - 'a']--){
+                    jyo += it;
+                }
+                v2[it - 'a'] = 0;
+            }
+        }
+        for (auto it : s){
+            if (v2[it - 'a'] >= 1){
+                jyo += it;
+                v2[it - 'a']--;
+            }
+        }
+        return jyo;
+    }
+};
