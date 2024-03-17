fd8efac28af723d3cc31ead24f417870cbcb104f
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-03-17 10:30:23 +0530

    Time: 8 ms (81.77%) | Memory: 21.1 MB (10.41%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<vector<int>> check(vector<vector<int>>&vec,int n){
+        vector<vector<int>>v;
+        int i = 0;
+        int mergeCount = 0;
+        while (i<n){
+            if (i==n-1){
+                v.push_back(vec[i]);
+                i++;
+            }
+            else{
+                if (vec[i][1] >= vec[i+1][0]){
+                    mergeCount++;
+                    int a = min(vec[i][0] , vec[i+1][0]);
+                    int b = max(vec[i][1] , vec[i+1][1]);
+                    v.push_back({a,b});
+                    i+=2;
+                }
+                else{
+                    v.push_back(vec[i]);
+                    i++;
+                }
+            }
+        }
+        if (mergeCount) return check(v,v.size());
+        return v;
+    }
+    vector<vector<int>> insert(vector<vector<int>>& vec, vector<int>& single) {
+        vector<vector<int>>v;
+        int i = 0;
+        int mergeCount = 0;
+        int n = vec.size();
+        int m = single.size();
+        if (n == 0) return {single};
+        if (m == 0) return vec;
+        if (vec[0][0] > single[0] and vec[n-1][1] < single[1]) return {single};
+        vec.push_back(single);
+        sort(vec.begin(),vec.end());
+        n++;
+        while (i<n){
+            if (i==n-1){
+                v.push_back(vec[i]);
+                i++;
+            }
+            else{
+                if (vec[i][1] >= vec[i+1][0]){
+                    mergeCount++;
+                    int a = min(vec[i][0] , vec[i+1][0]);
+                    int b = max(vec[i][1] , vec[i+1][1]);
+                    v.push_back({a,b});
+                    i+=2;
+                }
+                else{
+                    v.push_back(vec[i]);
+                    i++;
+                }
+            }
+        }
+        if (mergeCount) v = check(v,v.size());
+        return v;
+    }
+};
