0d7ea08eddc20dff918bfac9206872a436fa4dcf
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-03-25 18:09:37 +0530

    Time: 96 ms (98.25%) | Memory: 67.2 MB (54.08%) - LeetSync

---
+++ 148-sort-list/sort-list.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        ios_base::sync_with_stdio(0);
+        if (head == NULL or head->next == NULL){
+            return head;
+        }
+        vector<int>pos(5*1e4+1);
+        vector<int>neg(5*1e4+1);
+        int maxi = 0;
+        int mini = 0;
+        ListNode* temp = head;
+        while(temp != NULL){
+            if (temp->val < 0){
+                int value = 0 - temp->val;
+                neg[value]++;
+                if (mini > temp->val){
+                    mini = temp->val;
+                }
+            }
+            else{
+                pos[temp->val]++;
+                if (maxi < temp->val){
+                    maxi = temp->val;
+                }
+            }
+            temp = temp->next;
+        }
+        mini = 0-mini;
+        temp = head;
+        for (int i=mini;i>=1;i--){
+            if (neg[i] != 0){
+                while (neg[i]--){
+                    temp->val = -i;
+                    temp = temp->next;
+                }
+            }
+        }
+        for (int i=0;i<=maxi;i++){
+            if (pos[i] != 0){
+                while (pos[i]--){
+                    temp->val = i;
+                    temp = temp->next;
+                }
+            }
+        }
+        return head;
+    }
+};
