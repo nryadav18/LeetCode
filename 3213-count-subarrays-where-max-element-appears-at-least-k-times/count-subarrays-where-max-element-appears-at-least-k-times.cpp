a90d92689023af94405ee593d8658c8dab5deeff
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-03-29 12:20:56 +0530

    Time: 76 ms (98.45%) | Memory: 121.8 MB (46.90%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -7,15 +7,16 @@ public:
         if (k > n) return 0;
         int maxi = *max_element(nums.begin(),nums.end());
         long long res = 0;
+        unordered_map<int,int>up;
         int i = 0;
         int j = 0;
         int cnt = 0;
-        while ( j < n || i > j){
+        while ( j < n ){
             if (nums[j] == maxi){
-                cnt++;
-                while (cnt >= k){
+                up[nums[j]]++;
+                while (up[nums[j]] >= k and i<=j){
                     if (nums[i] == maxi){
-                        cnt--;
+                        up[nums[i]]--;
                     }
                     i++;
                     res += n-j;
