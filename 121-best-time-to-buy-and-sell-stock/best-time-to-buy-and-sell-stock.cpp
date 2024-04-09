2ed8ddbc6c833f8b8a6847a7f4c1f3950fa26c2f
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-04-09 15:16:57 +0530

    Time: 55 ms (98.89%) | Memory: 95.9 MB (31.33%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,30 +4,18 @@ public:
         ios_base::sync_with_stdio(0);
         cin.tie(nullptr);
         int n = prices.size();
-        vector<int>buy(n,-1);
-        int mini = INT_MAX;
-        bool flag = false;
-        for (int i = 0 ; i < n ; i++){
-            if (prices[i] < mini){
-                mini = prices[i];
-            }
-            buy[i] = mini;
-            if (!flag){
-                if (i != n-1 and prices[i] < prices[i+1]){
-                    flag = true;
-                }
-            }
-        }
-        if (!flag){
-            return 0;
-        }
+        int buy = INT_MAX;
         int val = 0;
+        int diff = 0;
         for (int i = 0 ; i < n ; i++){
-            if (buy[i] < prices[i]){
-                int diff = prices[i] - buy[i];
-                if (val < diff){
-                    val = diff;
-                }
+            if  (buy > prices[i]){
+                buy = prices[i];
+            }
+            if (prices[i] > buy){
+                diff = prices[i] - buy;
+            }
+            if (val < diff){
+                val = diff;
             }
         }
         return val;
