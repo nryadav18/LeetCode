32baec633111f8bcbcf3c208a427448942e77ba8
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-04-24 22:51:47 +0530

    Time: 3 ms (22.77%) | Memory: 7.3 MB (13.08%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
+    int funCall(int n,vector<int>&v){
+        if (n == 0) return 0;
+        if (n == 1 or n == 2) return 1;
+        if (v[n-1] == -1) v[n-1] = funCall(n-1,v);
+        if (v[n-2] == -1) v[n-2] = funCall(n-2,v);
+        if (v[n-3] == -1) v[n-3] = funCall(n-3,v);
+        v[n] = v[n-1]+v[n-2]+v[n-3];
+        return v[n];
+    }
     int tribonacci(int n) {
         ios_base::sync_with_stdio(0);
         cin.tie(nullptr);
-        if (n == 0) return 0;
-        if (n == 1 or n == 2) return 1;
-        int a=0,b=1,c=1;
-        long long d;
-        for(int i=3;i<=n;i++){
-            d = a+b+c;
-            a = b;
-            b = c;
-            c = d;
-        }
-        return d;
+        vector<int>v(n+1,-1);
+        int res = funCall(n,v);
+        return res;
     }
 };
