9932841675158c6acb15329648394d211be45916
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-04-24 22:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (5.38%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int funCall(int n,vector<int>&v){
+        ios_base::sync_with_stdio(0);
+        cin.tie(nullptr);
         if (n == 0) return 0;
         if (n == 1 or n == 2) return 1;
         if (v[n-1] == -1) v[n-1] = funCall(n-1,v);
