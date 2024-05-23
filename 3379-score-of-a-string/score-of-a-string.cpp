f36d312fffa3aa480c6cf2802e98e444291f8711
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-05-23 22:19:42 +0530

    Time: 2 ms (41.76%) | Memory: 7.9 MB (7.40%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(nullptr);
+        int n = s.length() , val = 0;
+        for (int i=0;i<n-1;i++) val += abs(s[i]-s[i+1]);
+        return val;
+    }
+};
