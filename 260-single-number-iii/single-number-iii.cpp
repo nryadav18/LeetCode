a2d2a4df9dc34625e0accb3d8d677ff4ec3f4fc8
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-05-31 16:57:42 +0530

    Time: 5 ms (79.18%) | Memory: 14.5 MB (10.01%) - LeetSync

--- 260-single-number-iii/single-number-iii.cpp
+++ 260-single-number-iii/single-number-iii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
+        ios_base::sync_with_stdio(0); //Beat Booster
         cin.tie(nullptr);
-        unordered_map<int,int>up;
-        int n = nums.size();
+        unordered_map<int,int>up; //An Unordered Map for O(1) Retreival of Elements
+        int n = nums.size(); 
         for (auto it : nums){
-            up[it]++;
+            up[it]++; //Adding elements into the Map
         }
-        vector<int>result;
+        vector<int>result; //Resultant Vector
         for (auto it : up){
-            if (it.second == 1){
-                result.push_back(it.first);
+            if (it.second == 1){ //Whenever the Frequency of an Element is 1
+                result.push_back(it.first); //Store its Original Number
             }
         }
-        return result;
+        return result; //Return the Resultant Vector
     }
 };
