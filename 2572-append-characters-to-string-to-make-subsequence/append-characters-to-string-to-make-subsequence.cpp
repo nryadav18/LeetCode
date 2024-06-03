e410e53f6086a8feb8e48b9781ad14a452857c0f
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-06-03 14:24:30 +0530

    Time: 12 ms (93.61%) | Memory: 11.9 MB (83.42%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        ios_base::sync_with_stdio(0);
+        ios_base::sync_with_stdio(0); //Beat Boosters
         cin.tie(nullptr);
-        int length = 0, n = s.length(), ind = 0;
+        int length = 0; //Length variable to find the total number of
+        int n = s.length(); // Common characters in both the strings S and T
+        int ind = 0; //Index to Iterate on string T
         for (int i=0;i<n;i++){
-            if (s[i] == t[ind]){
-                length++;
-                ind++;
+            if (s[i] == t[ind]){ //Whenever both Characters are equal in S and T
+                length++; //Increment length by 1 
+                ind++; //Increment index by one
             }
         }
-        int result = t.length() - length;
-        return result;
+        int result = t.length() - length; //Result would be obtained by 
+        return result; //subtracting common characters in both the Strings S and T
     }
 };
