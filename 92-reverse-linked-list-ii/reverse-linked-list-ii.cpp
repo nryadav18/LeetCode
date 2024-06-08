37a45ef7a7d45e6302947e7ff563e09170e010ac
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-06-08 14:11:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (13.90%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -9,34 +9,34 @@
  * };
  */
 class Solution {
-public:
+public: //Single Pass Solution
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ios_base::sync_with_stdio(0);
+        ios_base::sync_with_stdio(0); //Beats Booster
         cin.tie(nullptr);
         if (head->next == NULL or left == right) {
-            return head;
+            return head; //Base Case 
         }
-        ListNode* temp = head, *start = NULL;
+        ListNode* temp = head, *start = NULL; //pointers to maintain intial declarations
         while (temp != NULL and left-- > 1 and right-- > 1){
-            start = temp;
-            temp = temp->next;
+            start = temp; //maintain start pointer to connect after reversal
+            temp = temp->next; //temp pointer to find the intial reversal head node 
         }
         ListNode* prev = NULL, *cur = temp, *next = NULL;
-        while (cur != NULL and right--){
-            next = cur->next;
-            cur->next = prev;
-            prev = cur;
-            cur = next;
+        while (cur != NULL and right--){ //Basic LinkedList Reversal Technique
+            next = cur->next; //Maintain next of current node
+            cur->next = prev; //change the Links between them
+            prev = cur; //Assign the current node to previous node
+            cur = next; //Assign the next node to current node
         }
         if (start != NULL){
-            start->next = prev;
+            start->next = prev; //Connect Start next to prev
         }
         else{
-            head = prev;
+            head = prev; //First Major Edge Case to Handle
         }
         if (temp != NULL){
-            temp->next = cur;
+            temp->next = cur; //Second Major Edge Case to Handle
         }
-        return head;
+        return head; //Return Head of Final Modified Linked List After Reversal
     }
 };
