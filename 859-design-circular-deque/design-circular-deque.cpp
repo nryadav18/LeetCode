fd77f05b0b3573cdf2a55ad0163732d01ef00f26
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-09-28 21:24:04 +0530

    Time: 16 ms (87.95%) | Memory: 22.9 MB (17.67%) - LeetSync

--- 859-design-circular-deque/design-circular-deque.cpp
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -1,57 +1,90 @@
 class MyCircularDeque {
 public:
-    int size = -1;
-    deque<int>dq;
+    int List[1001];
+    int prev = 500, next = 500, maxSize = -1, listSize = 0;
     MyCircularDeque(int k) {
-        size = k;
+        for (int i=0;i<1001;i++){
+            List[i] = -1;
+        }
+        maxSize = k;
     }
     
     bool insertFront(int value) {
-        if (dq.size() < size){
-            dq.push_front(value);
-            return true;
+        if (listSize == maxSize){
+            return false;
         }
-        return false;
+        else if (listSize == 0){
+            List[prev] = value;
+        }
+        else{
+            List[--prev] = value;
+        }
+        listSize++;
+        return true;
     }
     
     bool insertLast(int value) {
-        if (dq.size() < size){
-            dq.push_back(value);
-            return true;
+        if (listSize == maxSize){
+            return false;
         }
-        return false;
+        else if (listSize == 0){
+            List[next] = value;
+        }
+        else{
+            List[++next] = value;
+        }
+        listSize++;
+        return true;
     }
     
     bool deleteFront() {
-        if (dq.size() > 0){
-            dq.pop_front();
-            return true;
+        if (listSize == 0){
+            return false;
         }
-        return false;
+        else if (listSize == 1){
+            List[prev] = -1;
+        }
+        else{
+            List[prev++] = -1;
+        }
+        listSize--;
+        return true;
     }
     
     bool deleteLast() {
-        if (dq.size() > 0){
-            dq.pop_back();
-            return true;
+        if (listSize == 0){
+            return false;
         }
-        return false;
+        else if (listSize == 1){
+            List[next] = -1;
+        }
+        else{
+            List[next--] = -1;
+        }
+        listSize--;
+        return true;
     }
     
-    int getFront() {
-        return (dq.size() > 0) ? dq.front() : -1;
+    int getFront() { 
+        if (listSize == 0){
+            return -1;
+        }
+        return List[prev];
     }
     
     int getRear() {
-        return (dq.size() > 0) ? dq.back() : -1;
+        if (listSize == 0){
+            return -1;
+        }
+        return List[next];
     }
     
     bool isEmpty() {
-        return dq.size() == 0;
+        return listSize == 0;
     }
     
     bool isFull() {
-        return dq.size() == size;
+        return listSize == maxSize;
     }
 };
 
