9951c5ab0a5b5ba586ed0c3c057e478c1a682d18
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-12-01 11:19:21 +0530

    Time: 10 ms (7.25%) | Memory: 15.4 MB (5.36%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    bool linearSearch(vector<int>v){
-        for (auto it : v){
-            if (it == 0){
-                return true;
-            }
-        }
-        return false;
-    }
     bool checkIfExist(vector<int>& arr) {
-        if (linearSearch(arr)){
-            int cnt = count(arr.begin(),arr.end(),0);
-            //cout << cnt;
-            if (cnt >= 2){
-                return true;
-            }
-            //return false;
-        }
+        ios_base::sync_with_stdio(0);
+        cin.tie(nullptr);
         map<int,int>mp;
+        for (auto it : arr) mp[it]++;
         for (auto it : arr){
-            mp[it]++;
-        }
-        for (auto it : mp){
-            int val = (it.first)*2;
-            if (val != 0 and mp.find(val) != mp.end()){
-                cout << val << " ";
-                return true;
+            if (it == 0 or it == 1){
+                if (mp[it] >= 2){
+                    return true;
+                }
+            }
+            else{
+                if (mp[it*2] >= 1){
+                    return true;
+                }
             }
         }
         return false;
