101eaa500f8d18450fa512b6a745c17efb45f84f
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-12-07 16:24:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (48.39%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -14,24 +14,17 @@ public:
     void performDFS(TreeNode* root,int target, vector<vector<int>>&result, int sum, vector<int>&store){
         if (root == NULL) return;
         sum += root->val;
-        cout << "a" << "  " << sum << endl;
+        // cout << "a" << "  " << sum << endl;
         store.push_back(root->val);
         performDFS(root->left,target,result,sum,store);
         if (sum == target and !root->left and !root->right){
             result.push_back(store);
         }
-        cout << "b" << " " << sum << endl;
+        // cout << "b" << " " << sum << endl;
         performDFS(root->right,target,result,sum,store);
         store.pop_back();
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        if (!root) return {};
-        if (!root->left and !root->right){
-            if (root->val == targetSum){
-                return {{root->val}};
-            }
-            return {};
-        }
         vector<vector<int>>result;
         vector<int>store;
         performDFS(root,targetSum,result,0,store);
