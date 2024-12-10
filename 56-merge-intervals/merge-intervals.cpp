91d1dfaa14d2f94506e772feb975a906568a8c6c
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2024-12-10 20:59:47 +0530

    Time: 7 ms (43.57%) | Memory: 24.9 MB (9.60%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-    bool check(vector<vector<int>>&v,int n){
-        if (n==1) return false;
-        int i=0;
-        int j=i+1;
-        while (i<n){
-            int val = v[i][1];
-            while (j<n){
-                if (v[j][0] <= v[i][1]){
-                    return true;
-                }
-                j++;
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>>result;
+        result.push_back(intervals[0]);
+        int n = intervals.size();
+        for (int i=1;i<n;i++){
+            auto last = result.back();
+            if (last[1] >= intervals[i][0]){
+                int len = result.size();
+                result[len-1][1] = max(result[len-1][1],intervals[i][1]);
             }
-            i++;
-            j=i+1;
-        }
-        return false;
-    }
-    vector<vector<int>> merge(vector<vector<int>>& v) {
-        ios_base::sync_with_stdio(0);
-        sort(v.begin(),v.end());
-        int n = v.size();
-        if (n==1) return v;
-        int i=0;
-        int j=i+1;
-        vector<vector<int>>res;
-        while (i<n){
-            int val = v[i][1];
-            while (j<n){
-                if (v[j][0] <= v[i][1]){
-                    val = max(v[j][1] , val);
-                    j++;
-                }
-                else{
-                    break;
-                }
+            else{
+                result.push_back(intervals[i]);
             }
-            res.push_back({v[i][0],val});
-            i = j;
-            j = i+1;
         }
-        if (!check(res,res.size())) return res;
-        return merge(res);
+        return result;
     }
 };
