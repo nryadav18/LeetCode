893eb1a9faa3a9c715267b2be1565c79a25ed046
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2025-07-01 14:42:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (10.92%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        int cnt = 1, n = word.length(), chCount = 0;
-        char ch = '#';
-        for (int i=n-1;i>=0;i--){
-            if (ch == '#'){
-                ch = word[i];
-                chCount++;
-            }
-            else if (ch != word[i]){
-                cnt += chCount - 1;
-                chCount = 1;
-                ch = word[i];
-            }
-            else{
-                chCount++;
-            }
-        }
-        if (chCount > 1) cnt += chCount - 1;
+        int n = word.length(), cnt = 1;
+        for (int i=0;i<n;i++) if (word[i] == word[i+1]) cnt++;
         return cnt;
     }
 };
