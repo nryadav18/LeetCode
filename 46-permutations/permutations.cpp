874b29940e58307d24ddca39d874af9451205227
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2025-07-23 12:23:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (39.04%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,21 +1,12 @@
 class Solution {
-    vector<vector<int>>mat;
-private:
-    void recFun(vector<int>v,int i,int j){
-        if (i >= j){
-            mat.push_back(v);
-            return;
-        }
-        for (int k = i ; k <= j ; k ++){
-            swap(v[i],v[k]);
-            recFun(v,i+1,j);
-            swap(v[i],v[k]);
-        }
-    }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        int n = nums.size();
-        recFun(nums,0,n-1);
-        return mat;
+        int n = nums.size(), val = tgamma(n+1);
+        vector<vector<int>>result;
+        for (int i = 0 ; i < val ; i++){
+            result.push_back(nums);
+            next_permutation(nums.begin(), nums.end());
+        }
+        return result;
     }
 };
