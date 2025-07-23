d30c7ef4ee5037b77d56d44f52f5aa349f9712a1
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2025-07-23 11:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (66.76%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(nullptr);
         int n = strs.size();
-        if (n == 1){
-            return strs[0]; //BaseCase 
-        }
-        string assumedPrefix = strs[0]; 
-        //A String -> AssumedPrefix which was intially
-        //            set to 0th Index of given Array of Strings
+        if (n == 1) return strs[0];
+        string answer = strs[0];
         for (int i=1;i<n;i++){
-            string temporaryPrefix = ""; //let TemporaryPrefix which is Empty;
-            if (strs[i] == ""){ //It indicates that there is NO Prefix in the Array;
-                return ""; //Whenever we encounter Empty String return it,
-            }
+            if (strs[i] == "") return "";
+            string newAnswer = "";
             for (int j=0;j<strs[i].length();j++){
-                if (assumedPrefix[j] != strs[i][j]){ 
-                    break; //Common Prefix got ended up, So Break down;
-                }
-                else{
-                    temporaryPrefix += strs[i][j]; //Add each character to get TempPrefix
+                if (strs[i][j] != answer[j]){
+                    break;
+                } else{
+                    newAnswer += answer[j];
                 }
             }
-            assumedPrefix = temporaryPrefix;//Update the AssumedPrefix for every Iteration
+            answer = newAnswer;
         }
-        return assumedPrefix; //Return the Assumed Prefix which is our Actual Prefix
+        return answer;
     }
 };
